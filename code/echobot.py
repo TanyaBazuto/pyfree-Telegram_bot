554ad352c8ec91f4a8299666fff77c927110895f
TanyaBazuto <150078962+TanyaBazuto@users.noreply.github.com>
2024-02-16 22:22:17 +0300

    Update echobot.py

--- code/echobot.py
+++ code/echobot.py
@@ -1,13 +1,22 @@
-import telebot
-
-token = ''
-
-bot = telebot.TeleBot(token)
+Для создания ботов для Telegram используются различные библиотеки, которые берут на себя взаимодействие с серверами Telegram и прочую
+служебную функциональность, оставляя пользователю написание логики ответа на сообщения. Таких библиотек для Python множество, мы будем использовать
+telebot https://github.com/eternnoir/pyTelegramBotAPI.
 
 
-@bot.message_handler(content_types=["text"])
-def echo(message):
-    bot.send_message(message.chat.id, message.text)
+import telebot                                            # в Pythonanywhere команда pip3 install --user pytelegrambotapi
 
 
-bot.polling(none_stop=True)
+token = ''                                                # Помещаем токен, который получили ранее, в переменную
+
+bot = telebot.TeleBot(token)                              # переменная bot - ключевая во всей программе. Это объект. Внутри него уже есть функции, которые мы будем вызывать.
+
+
+@bot.message_handler(content_types=["text"])              # Зарегистрировать def echo, как обработчик для каких-либо сообщений типа text (в данном случае -- всех сообщений, которые приходят).
+                                                            Специальный синтаксис, который указывает, какие именно сообщения будет обрабатывать определяемая дальше функция.
+    
+def echo(message):                                        # Определение функции echo, принимающей один параметр - сообщение (на которое мы и будем отвечать).
+    bot.send_message(message.chat.id, message.text)       # Тело функции. Отправляем сообщение в тот же чат, откуда получили (message.chat.id) с тем же текстом (message.text).
+
+
+bot.polling(none_stop=True)                               # функция, polling ПОСТОЯННО ОБРАЩАЕТСЯ К СЕРВЕРАМ ТЕЛЕГРАМ - начинает отправку запросов серверам Телеграм, используя токен, и спрашивает есть ли сообщения.
+                                                            Если есть обращается к обработке - def echo.
